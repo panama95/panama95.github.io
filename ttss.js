79dc31f6b16b80d1ac64cafded0ab2db3b0abf46
panama95 <88645866+panama95@users.noreply.github.com>
2022-10-12 11:33:18 -0500

    Add files via upload

---
+++ ttss.js
@@ -0,0 +1,82 @@
+var ttss = [
+	{	//1
+		"word":"ieee",
+		"clue":"Segun quien la calidad de software es cuando el software satisface las expectativas del usuario. "
+	},
+	{	//2
+		"word":"validacion",
+		"clue":"Comprueba que las funcionalidades operan como el cliente espera "
+	},
+	{	//3
+		"word":"verificacion",
+		"clue":"Comprueba que el software funciona "
+	},
+	{	//4
+		"word":"garantia",
+		"clue":"Son un conjunto de acciones planificadas y sistemáticas "
+	},
+	{	//5
+		"word":"calidad",
+		"clue":"Esta centrada en el proceso y tambien depende de la cantidad que el cliente este dispuesto a pagar "
+	},
+	{	//6
+		"word":"fiabilidad",
+		"clue":"Involucra que la probabilidad de que un producto no falle en un periodo de tiempo determinado "
+	},
+	{	//7
+		"word":"estetica",
+		"clue":"Se mantiene subjetiva y relacionada a la apariencia del software. "
+	},
+	{	//8
+		"word":"percibida",
+		"clue":"Calidad que depende de la subjetividad del cliente "
+	},
+	{	//9
+		"word":"industrializacion",
+		"clue":"Revolucion donde el trabajo manual fue remplazado por el trabajo mecanico. "
+	},
+	{	//10
+		"word":"estrategias",
+		"clue":"Evolucion historica donde los sistemas de gestion se consolidan y la implementacion del personal aumenta. "
+	},
+	{	//11
+		"word":"Introducir los cambios y documentacion es un proceso de ____",
+		"clue":"Esta centrada en el proceso y tambien depende de la cantidad que el cliente este dispuesto a pagar "
+	},
+	{	//12
+		"word":"fabricante",
+		"clue":"Se examina durante la produccion y despues de la entrega  "
+	},
+	{	//13
+		"word":"producto",
+		"clue":"Se centra en la medida de los atributos de la produccion "
+	},
+	{	//14
+		"word":"usuario",
+		"clue":"La calidad es adecuada al proposito  "
+	},
+	{	//15
+		"word":"trascendental",
+		"clue":"Es la idea al que se intenta llegar, aunque no es conseguido debido a deficiencias en la tecnologia"
+	},
+	{	//16
+		"word":"conformidad",
+		"clue":"Grado donde el diseño y las prestaciones del producto cumplen "
+	},
+	{	//17
+		"word":"planificada",
+		"clue":"Tipo de calidad que pretende obtener y esta formalizada en documentos de proyecto "
+	},
+	{	//18
+		"word":"toyota",
+		"clue":"Compania Japonesa que reconocio un fallo de software en su modelo "
+	},
+	{	//19
+		"word":"integracion",
+		"clue":"Se espera que la inteligencia artificial mejore aun mas el proceso de: "
+	},
+	{	//20
+		"word":"esperada",
+		"clue":"La calidad que potencia con una adecuada obtencion de informacion  "
+	},
+]
