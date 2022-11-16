ef2e0b635f8d8d38e86a24875f1dbdf0f06edc9a
roningg48 <gael.gomez@utp.ac.pa>
2022-11-16 14:09:21 -0500

    cambios2

--- ttss.js
+++ ttss.js
@@ -13,7 +13,7 @@ var ttss = [
 	},
 	{	//4
 		"word":"garantia",
-		"clue":"Son un conjunto de acciones planificadas y sistemáticas "
+		"clue":"Son un conjunto de acciones planificadas y sistematicas "
 	},
 	{	//5
 		"word":"calidad",
@@ -79,4 +79,86 @@ var ttss = [
 		"word":"esperada",
 		"clue":"La calidad que potencia con una adecuada obtencion de informacion  "
 	},
+	{	//21
+		"word":"Cuatro",
+		"clue":"Cuantos pasos lleva el proceso de aseguramiento de la calidad. "
+	},
+	{	//22
+		"word":"Harvard ",
+		"clue":"¿De que universidad era profesor David Garbin? "
+	},
+	{	//23
+		"word":"Mecanico",
+		"clue":"¿Que reemplazo al trabajo manual en la revolucion industrial? "
+	},
+	{	//24
+		"word":"Aseguramiento",
+		"clue":"¿Introducir los cambios y documentarlos es un paso del proceso de? "
+	},
+	{	//25
+		"word":"Cinco",
+		"clue":"Cuantas vistas tiene la calidad segun David Garbin. "
+	},
+	{	//26
+		"word":"Producto",
+		"clue":"Una de las cinco vistas de la calidad segun David Garbin. "
+	},
+	{	//27
+		"word":"Seis",
+		"clue":"¿Cuantas dimensiones de la calidad de software existen? "
+	},
+	{	//28
+		"word":"Esperada",
+		"clue":"Es un tipo de calidad de software  "
+	},
+	{	//29
+		"word":"Cliente",
+		"clue":"¿En el fondo muchos problemas de calidad pueden tener su origen en falsas expectativas por parte del? "
+	},
+	{	//30
+		"word":"Calculadora",
+		"clue":"Un ejemplo de diferenciacion es la inclusion de una ----- en un software de contabilidad "
+	},
+	{	//31
+		"word":"Direccion",
+		"clue":"¿La calidad ya no es impulsada por inspectores, sino por la? "
+	},
+	{	//32
+		"word":"Consoliden",
+		"clue":"¿Si la implicacion del personal aumenta permite que los sistemas de gestion se? "
+	},
+	{	//33
+		"word":"Joseph",
+		"clue":"precursor en la evolucion de conceptos de calidad, considerado experto en la gestion y calidad del software  "
+	},
+	{	//34
+		"word":"productividad",
+		"clue":"una de las ventajas que aportan las empresas a la evolucion de la calidad es la mejora de "
+	},
+	{	//35
+		"word":"Prius",
+		"clue":"Nombre del software de Toyota que tuvo un fallo de perdidas millonarias  "
+	},
+	{	//36
+		"word":"Linea",
+		"clue":"El uso de las telecomunicaciones permitira brindar soporte en "
+	},
+	{	//37
+		"word":"Estadistico",
+		"clue":"¿Los primeros sistemas de la evolucion historia de la calidad de software tenian un control? "
+	},
+	{	//38
+		"word":"Estrategias",
+		"clue":"En 1980-1990 se representan las  "
+	},
+	{	//39
+		"word":"Total",
+		"clue":"Todo forma parte de un nuevo proceso llamado Calidad. "
+	},
+	{	//40
+		"word":"Telecomunicaciones",
+		"clue":"El futuro de la calidad de software es el uso de  "
+	},
+
+
 ]
